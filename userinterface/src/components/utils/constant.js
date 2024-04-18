c2e87214aedc9f2a30bbedcd16e5cfb6a8ebd410
Suhasp987 <ps9781094@gmail.com>
2024-04-18 23:50:39 +0530

    cg

--- userinterface/src/components/utils/constant.js
+++ userinterface/src/components/utils/constant.js
@@ -1,13 +1,13 @@
 // import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import HomeIcon from '@mui/icons-material/Home';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import HomeIcon from '@mui/icons-material/Home';
 
 
 
 
 export const Categories=[
-         {name:"Home",  icon:<HomeIcon/>,},
+         {name:"Home",  icon:Home,},
          {name:"Cart" ,icon:"Hello",},
-         {name:'Account',icon:<AccountCircleIcon/>,}
+         {name:'Account',icon:Account,}
          
 ]
