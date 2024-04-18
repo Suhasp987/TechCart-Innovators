9e86f6de06272fd9d6c4244b50c9a3dd32c0a15f
Suhasp987 <ps9781094@gmail.com>
2024-04-19 00:03:58 +0530

    v

--- userinterface/src/components/ItemCard.jsx
+++ userinterface/src/components/ItemCard.jsx
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from "react";
-
+import Razorpay from 'razorpay';
 import "./ItemCard.css";
 import { Button } from "@mui/material";
 import Items from "./items";
